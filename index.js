32485fb2091d111fa0253ca588c2f26832ef0080
Yurii <webdev56@gmail.com>
2022-01-31 13:18:14 +0300

    add btn theme

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const counter = document.getElementById('counter');
 const addBtn = document.getElementById("add");
 const subBtn = document.getElementById("sub");
 const asyncBtn = document.getElementById("async");
+const themeBtn = document.getElementById("theme");
 
 let state = 0;
 
@@ -20,7 +21,11 @@ asyncBtn.addEventListener('click', function () {
         state++;
         render();
     }, 1000);
-})
+});
+
+themeBtn.addEventListener('click', function () {
+    document.body.classList.toggle('dark');
+});
 
 
 function render() {
