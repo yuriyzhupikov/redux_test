6d867cee92c10660509b7f7fcee6a118a043f92e
Yurii <webdev56@gmail.com>
2022-01-31 14:21:12 +0300

    added types actions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {createStore} from "./createStore.js";
 // import {createStore} from "redux";
 import {rootReducer} from "./redux/rootReducer.js";
+import {INCREMENT, DECREMENT} from "./redux/types.js";
 
 const addBtn = document.getElementById("add");
 const subBtn = document.getElementById("sub");
@@ -10,11 +11,11 @@ const themeBtn = document.getElementById("theme");
 const store = createStore(rootReducer, 0);
 
 addBtn.addEventListener('click', function () {
-    store.dispatch({type: "INCREMENT"});
+    store.dispatch({type: INCREMENT});
 });
 
 subBtn.addEventListener('click', function () {
-    store.dispatch({type: "DECREMENT"});
+    store.dispatch({type: DECREMENT});
 });
 
 asyncBtn.addEventListener('click', function () {
--- redux/rootReducer.js
+++ redux/rootReducer.js
@@ -1,7 +1,9 @@
+import {INCREMENT, DECREMENT} from "./types.js";
+
 export function rootReducer(state, action) {
     switch (action.type) {
-        case "INCREMENT": return state + 1;
-        case "DECREMENT": return state - 1;
+        case INCREMENT: return state + 1;
+        case DECREMENT: return state - 1;
     }
     return state
 }
