9307ae1e2731ade0a76c696126320a0e4f68925b
Yurii <webdev56@gmail.com>
2022-01-31 13:30:37 +0300

    separation into component and state

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
-const counter = document.getElementById('counter');
 const addBtn = document.getElementById("add");
 const subBtn = document.getElementById("sub");
 const asyncBtn = document.getElementById("async");
 const themeBtn = document.getElementById("theme");
 
-let state = 0;
-
 addBtn.addEventListener('click', function () {
-    state++;
-    render();
+
 });
 
 subBtn.addEventListener('click', function () {
-    state--;
-    render();
+
 });
 
 asyncBtn.addEventListener('click', function () {
-    setTimeout(function (){
-        state++;
-        render();
-    }, 1000);
+
 });
 
 themeBtn.addEventListener('click', function () {
-    document.body.classList.toggle('dark');
+
 });
-
-
-function render() {
-    counter.textContent = state.toString();
-}
-
-render();
